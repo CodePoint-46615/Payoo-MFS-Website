650b28990feb4a7157be6717a75aa239d4db18c6
CodePoint-46615 <codepoint46615@gmail.com>
2025-08-30 15:01:04 +0600

    complete-login-system

---
+++ script.js
@@ -0,0 +1,19 @@
+// login button functionality
+document.getElementById('btn-login').addEventListener('click', (event)=>{
+    event.preventDefault(); 
+    const mobileNumber = 12345678910; 
+    const pin = 1234; 
+
+    const userGivenMobileNumberValue = document.getElementById('user-mobile').value;
+    const userGivenMobileNumber = parseInt(userGivenMobileNumberValue); 
+
+    const userGivenPinNumberValue = document.getElementById('user-pin').value; 
+    const userGivenPinNumber = parseInt(userGivenPinNumberValue);
+    
+    if(mobileNumber === userGivenMobileNumber && pin === userGivenPinNumber){
+        window.location.href = './home.html'; 
+    }
+    else{
+        alert('Invalid Credentials. Try Again');
+    }
+})
